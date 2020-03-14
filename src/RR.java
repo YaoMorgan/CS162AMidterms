852168e03b9166bfebb8fe3836114adffcc062ca
MorganYao <morgan.yao@obf.ateneo.edu>
2020-03-14 20:58:20 +0800

    completed rr algo

--- src/RR.java
+++ src/RR.java
@@ -1,57 +1,39 @@
 public class RR {
-	static void findWaitingTime(int numP, int[] bt, int[] wt, int rrTime) {
-		int curbt[] = new int[numP];
+	private int timeElapsed, cpuBurst;
+	private int[]  waitingTime, turnAround, responseTime;
+	private double throughput, cpuUtil, averageWaitingTime, averageTurnAround, averageResponseTime;
+	public void run(int[][] process, int lines, int rrTime) {
+		int[] waitingTime = new int[lines];
+		int[] turnAround = new int[lines];
+		int[] responseTime = new int[lines];
 		
-		//Copy burst times
-		for(int i = 0; i < numP; i++) {
-			curbt[i] = bt[i];
+		int remainingBurst[]=new int[lines];
+		for(int i=0; i<lines; i++) {
+			remainingBurst[i]=process[i][1];
 		}
-		
-		int counter = 0;	//timer
-		
 		while(true) {
-			boolean done = true;
-			// Traverse all processes one by one repeatedly
-			for(int i = 0; i < numP; i++) {
-				//If bt of a process is greater than 0 it needs to be processed
-				if(curbt[i] > 0) {
-					done = false;	//process is on-going
-					if(curbt[i] > rrTime) {
-						//increase time based on the rrTime
-						counter += rrTime;
-						//decrease bt of current process by rrTime
-						curbt[i] -= rrTime;
+			boolean done=true;
+			for(int i=0; i<lines; i++) {
+				if(remainingBurst[i]>0) {
+					done=false;
+					if(remainingBurst[i]>rrTime) {
+						//add the given time and reduce remaining
+						timeElapsed+=rrTime;
+						remainingBurst[i]-=rrTime;
+						//System.out.printf("Process%d switched at %dns\n",process[i][3],timeElapsed);
 					}
-					// If bt is less than or equal to rrTime, it's the last cycle of the process
-					else { 
-						//Increase time based on how long the process is
-						counter += curbt[i];
-						//wt = current time - time used by the process
-						wt[i] = counter - bt[i];
-						//Make bt 0 after full execution
-						curbt[i] = 0;
+					else {
+						//add the remaining time
+						timeElapsed+=remainingBurst[i];
+						//how long the process waiting before it ended
+						waitingTime[i]=timeElapsed-process[i][1];
+						remainingBurst[i]=0;
+						//System.out.printf("Process%d ended in %dns\n",process[i][3],timeElapsed);
 					}
 				}
 			}
-			
-			if(done == true) {
+			if(done==true)
 				break;
-			}
 		}
-	}
-	
-	static void findTurnAroundTime(int n, int[] bt, int[] wt, int[] tat) {
-		//tat = bt + wt
-		for(int i = 0; i < n; i++) {
-			tat[i] = bt[i] + wt[i];
-		}
-	}
-	
-	static void findAveTime(int numP, int[] bt, int rrTime) {
-		
-	}
-	
-	static void RR(int rrTime) {
-		
 	}
 }
